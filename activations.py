13c8ff1b272d1ba17bb41d0e976beece6e21feee
Nizar Benbouchta <nizarbenbouchta@Nizars-MacBook-Air.local>
2025-01-30 23:58:45 +0100

    fixing activations and adding basic tests

--- activations.py
+++ activations.py
@@ -8,11 +8,11 @@ class ReLU(Function):
     @staticmethod
     def apply(a):
         a = a if isinstance(a, Tensor) else Tensor(np.array(a))
+        relu = ReLU()
+        relu.inputs = [a]
         out_data = np.maximum(0, a.data)
-        out = Tensor(out_data, requires_grad=a.requires_grad)
+        out = Tensor(out_data, requires_grad=a.requires_grad, depends_on=[relu])
         if out.requires_grad:
-            relu = ReLU()
-            relu.inputs = [a]
             relu.output = out
             out.set_grad_fn(relu)
         return out
@@ -24,16 +24,15 @@ class ReLU(Function):
         grad_a = broadcast_backward(grad_a, a.data.shape)
         return (grad_a,)
 
-
 class Sigmoid(Function):
     @staticmethod
     def apply(a):
         a = a if isinstance(a, Tensor) else Tensor(np.array(a))
+        sigmoid = Sigmoid()
+        sigmoid.inputs = [a]
         out_data = 1 / (1 + np.exp(-a.data))
-        out = Tensor(out_data, requires_grad=a.requires_grad)
+        out = Tensor(out_data, requires_grad=a.requires_grad, depends_on=[sigmoid])
         if out.requires_grad:
-            sigmoid = Sigmoid()
-            sigmoid.inputs = [a]
             sigmoid.output = out
             out.set_grad_fn(sigmoid)
         return out
@@ -49,17 +48,37 @@ class Tanh(Function):
     @staticmethod
     def apply(a):
         a = a if isinstance(a, Tensor) else Tensor(np.array(a))
+        tanh = Tanh()
+        tanh.inputs = [a]
         out_data = np.tanh(a.data)
-        out = Tensor(out_data, requires_grad=a.requires_grad)
+        out = Tensor(out_data, requires_grad=a.requires_grad, depends_on=[tanh])
         if out.requires_grad:
-            tanh = Tanh()
-            tanh.inputs = [a]
             tanh.output = out
             out.set_grad_fn(tanh)
         return out
 
     def backward(self, grad_output):
         a = self.inputs[0]
-        grad_a = grad_output * (1 - self.output.data**2)
+        grad_a = grad_output * (1 - self.output.data ** 2)
         grad_a = broadcast_backward(grad_a, a.data.shape)
         return (grad_a,)
+
+class Softmax(Function):
+    @staticmethod
+    def apply(a):
+        a = a if isinstance(a, Tensor) else Tensor(np.array(a))
+        softmax = Softmax()
+        softmax.inputs = [a]
+        out_data = np.exp(a.data) / np.exp(a.data).sum(axis=-1, keepdims=True)
+        out = Tensor(out_data, requires_grad=a.requires_grad, depends_on=[softmax])
+        if out.requires_grad:
+            softmax.output = out
+            out.set_grad_fn(softmax)
+        return out
+
+    def backward(self, grad_output):
+        a = self.inputs[0]
+        grad_a = grad_output * self.output.data * (1 - self.output.data)
+        grad_a = broadcast_backward(grad_a, a.data.shape)
+        return (grad_a,)
+
